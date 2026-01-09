d20fa8e77dad5451f954b3f20a15cb5c9cca1256
fetost <patranikap@gmail.com>
2026-01-09 12:07:22 +0100

    Added more for the setup

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Net.Mime;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
@@ -7,6 +8,7 @@ namespace DungeonSlime;
 
 public class Game1 : Core
 {
+    private Texture2D _monologo;
     public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
 
@@ -22,8 +24,9 @@ public class Game1 : Core
     protected override void LoadContent()
     {
         // TODO: use this.Content to load your game content here
+        _monologo = Content.Load<Texture2D>("images/monologo");
 
-        base.LoadContent();
+        //base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
@@ -41,6 +44,14 @@ public class Game1 : Core
         GraphicsDevice.Clear(Color.Pink);
 
         // TODO: Add your drawing code here
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin();
+
+        // Draw the logo texture
+        SpriteBatch.Draw(_monologo, Vector2.Zero, Color.White);
+
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
 
         base.Draw(gameTime);
     }
